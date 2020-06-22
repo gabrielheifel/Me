4d72d721b24c9166fb2113de42018f026c38d774
GHeifel <gabrielheifel@gmail.com>
2020-06-22 13:46:41 -0300

    theme add

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,20 @@ const showMenu = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
 
-    burger.addEventListener('click', () =>{
+    burger.addEventListener('click', () => {
         nav.classList.toggle('show-menu');
 
         burger.classList.toggle('burger-animation');
     });
 }
 
+const turnOn = () => {
+    const theme = document.querySelector('.theme');
+
+    theme.addEventListener('click', () => {
+        document.body.classList.toggle('light');
+    });
+}
+
 showMenu();
+turnOn();
