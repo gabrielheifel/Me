49e919c1849ab115d0067052b2c9ea293ea3a6a3
GHeifel <gabrielheifel@gmail.com>
2020-06-29 18:25:08 -0300

    skills ok

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,5 +20,28 @@ const turnOn = () => {
     });
 }
 
+const opSkill = () => {
+    const opMe = document.querySelector('.me');
+    const opWith = document.querySelector('.with');
+    const skills = document.querySelectorAll('.skill-container');
+
+    opMe.addEventListener('click', () => {
+        opMe.style.border = "2px solid #4e1116"
+        for(const skill of skills){
+            skill.classList.remove('with-gs');
+            skill.classList.add('without-gs');
+        }
+    })
+
+    opWith.addEventListener('click', () => {
+        opMe.style.border = "none";
+        for(const skill of skills){
+            skill.classList.remove('without-gs');
+            skill.classList.add('with-gs');
+        }
+    })
+}
+
 showMenu();
 turnOn();
+opSkill();
