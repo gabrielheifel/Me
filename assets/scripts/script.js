32bef0591cd02e74214da25780f0dab848f6d1de
GHeifel <gabrielheifel@gmail.com>
2020-06-30 15:28:25 -0300

    option-skill ok

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,21 +20,23 @@ const turnOn = () => {
     });
 }
 
+// ---- SKILLS ----
 const opSkill = () => {
     const opMe = document.querySelector('.me');
     const opWith = document.querySelector('.with');
-    const skills = document.querySelectorAll('.skill-container');
+    const skills = document.querySelectorAll('.skill-container, .option-skill');
 
     opMe.addEventListener('click', () => {
-        opMe.style.border = "2px solid #4e1116"
+
         for(const skill of skills){
             skill.classList.remove('with-gs');
             skill.classList.add('without-gs');
         }
+
     })
 
     opWith.addEventListener('click', () => {
-        opMe.style.border = "none";
+
         for(const skill of skills){
             skill.classList.remove('without-gs');
             skill.classList.add('with-gs');
